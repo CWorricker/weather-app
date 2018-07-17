a1386162cbbe70d9e9e5a50cf380cf25a502b15e
CWorricker <carl.worricker@gmail.com>
2018-07-17 20:13:12 +0100

    added 5 day, background

--- js/index.js
+++ js/index.js
@@ -7,7 +7,6 @@ let tempMin = document.getElementById("temp-min");
 let tempMax = document.getElementById("temp-max");
 let temperature = document.getElementById("temp");
 let country = document.getElementById("country");
-//let wind = document.getElementById("wind");
 let weatherDesc = document.getElementById("weather-desc");
 let weatherId = document.getElementById("weather-id");
 
@@ -29,24 +28,28 @@ function findWeatherDetails() {
 			"&appid=" +
 			appKey;
 		httpRequestAsync(searchLink, theResponse);
+
+		let searchLink2 =
+			"https://api.openweathermap.org/data/2.5/forecast?q=" +
+			searchInput.value +
+			"&appid=" +
+			appKey;
+		httpRequestAsync(searchLink2, theResponse2);
 	}
 }
 
 function theResponse(response) {
 	let jsonObject = JSON.parse(response);
 	cityName.innerHTML = jsonObject.name;
-	// icon.src = "http://openweathermap.org/img/w/" + jsonObject.weather[0].icon + ".png";
 	temperature.innerHTML = parseInt(jsonObject.main.temp - 273);
 	tempMin.innerHTML = parseInt(jsonObject.main.temp_min - 273);
 	tempMax.innerHTML = parseInt(jsonObject.main.temp_max - 273);
 	country.innerHTML = jsonObject.sys.country;
-	//wind.innerHTML = jsonObject.wind.speed;
 	weatherDesc.innerHTML = jsonObject.weather[0].description;
 	weatherId.innerHTML = jsonObject.weather[0].id;
 
 	//icon
 	var icon;
-
 	switch (weatherId.innerHTML) {
 		case "200":
 		case "201":
@@ -56,14 +59,12 @@ function theResponse(response) {
 		case "232":
 			icon = "wi wi-storm-showers";
 			break;
-
 		case "210":
 		case "211":
 		case "212":
 		case "221":
 			icon = "wi wi-thunderstorm";
 			break;
-
 		case "300":
 		case "301":
 		case "302":
@@ -75,7 +76,6 @@ function theResponse(response) {
 		case "321":
 			icon = "wi wi-rain-mix";
 			break;
-
 		case "500":
 		case "501":
 		case "502":
@@ -88,7 +88,6 @@ function theResponse(response) {
 		case "531":
 			icon = "wi wi-rain";
 			break;
-
 		case "600":
 		case "601":
 		case "602":
@@ -101,7 +100,6 @@ function theResponse(response) {
 		case "622":
 			icon = "wi wi-snow";
 			break;
-
 		case "701":
 		case "711":
 		case "721":
@@ -114,28 +112,24 @@ function theResponse(response) {
 		case "781":
 			icon = "wi wi-fog";
 			break;
-
 		case "800":
 			icon = "wi wi-day-sunny";
 			break;
-
 		case "801":
 		case "802":
 		case "803":
 			icon = "wi wi-cloud";
 			break;
-
 		case "804":
 			icon = "wi wi-cloudy";
 			break;
-
 		default:
 			icon = "wi wi-na";
 	}
 	document.getElementById("image").innerHTML = '<i class="' + icon + '"></i>';
 
 	//temp - color
-	if (temperature.innerHTML > 20) {
+	if (temperature.innerHTML >= 20) {
 		$("html, #image").removeClass();
 		$("html, #image").addClass("hot");
 	} else if (temperature.innerHTML < 20 && temperature.innerHTML > 15) {
@@ -145,10 +139,113 @@ function theResponse(response) {
 		$("html, #image").removeClass();
 		$("html, #image").addClass("cold");
 	}
+	
+}
+
+function theResponse2(response2) {
+	let jsonObject = JSON.parse(response2);
+
+	var i;
+	var text = "";
+	for (i = 7; i < jsonObject.list.length; i = i + 8) {
+		var timestamp = jsonObject.list[i].dt;
+		var a = new Date(timestamp * 1000);
+		var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+		var dayOfWeek = days[a.getDay()];
+
+		var forecastId = jsonObject.list[i].weather[0].id;
+		var icon2;
+
+		switch (forecastId) {
+			case 200:
+			case 201:
+			case 202:
+			case 230:
+			case 231:
+			case 232:
+				icon2 = "wi wi-storm-showers";
+				break;
+			case 210:
+			case 211:
+			case 212:
+			case 221:
+				icon2 = "wi wi-thunderstorm";
+				break;
+			case 300:
+			case 301:
+			case 302:
+			case 310:
+			case 311:
+			case 312:
+			case 313:
+			case 314:
+			case 321:
+				icon2 = "wi wi-rain-mix";
+				break;
+			case 500:
+			case 501:
+			case 502:
+			case 503:
+			case 504:
+			case 511:
+			case 520:
+			case 521:
+			case 522:
+			case 531:
+				icon2 = "wi wi-rain";
+				break;
+			case 600:
+			case 601:
+			case 602:
+			case 611:
+			case 612:
+			case 615:
+			case 616:
+			case 620:
+			case 621:
+			case 622:
+				icon2 = "wi wi-snow";
+				break;
+			case 701:
+			case 711:
+			case 721:
+			case 731:
+			case 741:
+			case 751:
+			case 761:
+			case 762:
+			case 771:
+			case 781:
+				icon2 = "wi wi-fog";
+				break;
+			case 800:
+				icon2 = "wi wi-day-sunny";
+				break;
+			case 801:
+			case 802:
+			case 803:
+				icon2 = "wi wi-cloud";
+				break;
+			case 804:
+				icon2 = "wi wi-cloudy";
+				break;
+			default:
+				icon2 = "wi wi-na";
+		}
+
+		text +=
+			"<div id='forecastDay'>" + 
+			dayOfWeek +
+			"</div><div class='inline' id='forecastIcon' class='inline'><i class='" +
+			icon2 +
+			"'></i></div><div class='inline' id='forecastTemp'>" +
+			parseInt(jsonObject.list[i].main.temp - 273) +
+			"<span class='deg'>°c</span></div><br />";
+	}
+	document.getElementById("forecast").innerHTML = text;
 }
 
 function httpRequestAsync(url, callback) {
-	console.log("hello");
 	var httpRequest = new XMLHttpRequest();
 	httpRequest.onreadystatechange = () => {
 		if (httpRequest.readyState == 4 && httpRequest.status == 200)
@@ -157,3 +254,23 @@ function httpRequestAsync(url, callback) {
 	httpRequest.open("GET", url, true); // true for asynchronous
 	httpRequest.send();
 }
+
+$(function() {
+  let builderLink = "https://source.unsplash.com/1600x900/?";
+  $('input').on('change', () => {
+    //$('img').fadeOut(2000);
+    $('img').attr('src', builderLink + $('input').val());
+    //$('img').fadeIn(2000);
+  });
+});
+
+
+
+// TO DO //
+// celcius to farenheit swap
+// drop down option UI - auto complete or something?
+// day time night time
+// default location? geolocation?
+// dynamically give credit to unsplash images
+// draggable UI maybe? why?
+// responsiveness
